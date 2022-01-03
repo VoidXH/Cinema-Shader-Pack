85076c54d715d272d9d293110e1c804001a06310
Bence Sgánetz <voidxb@gmail.com>
2022-01-03 17:11:14 +0100

    Hue retain on HDR clipping

--- Shaders/HDR to SDR.hlsl
+++ Shaders/HDR to SDR.hlsl
@@ -8,9 +8,9 @@
 */
 
 // Configuration ---------------------------------------------------------------
-const static float peakLuminance = 100.0; // Peak playback screen luminance in nits
+const static float peakLuminance = 250.0; // Peak playback screen luminance in nits
 const static float knee = 0.75; // Compressor knee position
-const static float ratio = 5.0; // Compressor ratio: 1 = disabled, <1 = expander
+const static float ratio = 1.0; // Compressor ratio: 1 = disabled, <1 = expander
 const static float maxCLL = 10000.0; // Maximum content light level in nits
 // -----------------------------------------------------------------------------
 
@@ -27,13 +27,43 @@ const static float c3 = 2392 / 128.0;
 
 sampler s0;
 
-inline float peakGain(float3 pixel) {
+inline float minGain(float3 pixel) {
+  return min(pixel.r, min(pixel.g, pixel.b));
+}
+
+inline float midGain(float3 pixel) {
+  return pixel.r < pixel.g ?
+    (pixel.r < pixel.b ?
+      min(pixel.g, pixel.b) : // min = r
+      min(pixel.r, pixel.g)) : // min = b
+    (pixel.g < pixel.b ?
+      min(pixel.r, pixel.b) : // min = g
+      min(pixel.r, pixel.g)); // min = b
+}
+
+inline float maxGain(float3 pixel) {
   return max(pixel.r, max(pixel.g, pixel.b));
 }
 
 inline float3 compress(float3 pixel) {
-  float gain = peakGain(pixel);
+  float gain = maxGain(pixel);
   return pixel * (gain < knee ? gain : knee + max(gain - knee, 0) * compressor) / gain;
+}
+
+inline float3 fixClip(float3 pixel) {
+  // keep the (mid - min) / (max - min) ratio
+  float preMin = minGain(pixel);
+  float preMid = midGain(pixel);
+  float preMax = maxGain(pixel);
+  float3 clip = saturate(pixel);
+  float postMin = minGain(clip);
+  float postMid = midGain(clip);
+  float postMax = maxGain(clip);
+  float ratio = (preMid - preMin) / (preMax - preMin);
+  float newMid = ratio * (postMax - postMin) + postMin;
+  return float3(clip.r != postMid ? clip.r : newMid,
+                clip.g != postMid ? clip.g : newMid,
+                clip.b != postMid ? clip.b : newMid);
 }
 
 inline float3 pq2lin(float3 pq) { // Returns luminance in nits
@@ -52,8 +82,8 @@ inline float3 lin2srgb(float3 lin) {
 
 inline float3 bt2020to709(float3 bt2020) { // in linear space
   return float3(
-    bt2020.r * 1.6605 + bt2020.g * -0.5876 + bt2020.b * -0.0728,
-    bt2020.r * -0.1246 + bt2020.g * 1.1329 + bt2020.b * -0.0083,
+    bt2020.r *  1.6605 + bt2020.g * -0.5876 + bt2020.b * -0.0728,
+    bt2020.r * -0.1246 + bt2020.g *  1.1329 + bt2020.b * -0.0083,
     bt2020.r * -0.0182 + bt2020.g * -0.1006 + bt2020.b * 1.1187);
 }
 
@@ -61,5 +91,5 @@ float4 main(float2 tex : TEXCOORD0) : COLOR {
   float3 pxval = tex2D(s0, tex).rgb;
   float3 lin = bt2020to709(pq2lin(pxval));
   float3 final = lin2srgb(compress(lin));
-  return final.rgbb;
+  return fixClip(final).rgbb;
 }
